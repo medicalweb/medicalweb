7d728989ff433ad86c9fe038a6da5dc8c0f42de5
Jeremy Lynch <jeremylynch1994@gmail.com>
2017-01-08 19:47:14 +1000

    fixed position for heart on home

--- assets/javascript/vendor/medicalweb.js
+++ assets/javascript/vendor/medicalweb.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     strings: [
     	"Hi.", 
     	"We are a group of Medical Students", 
-    	"At Griffith University, Gold Coast",
+    	"At <strong>Griffith University</strong>, Gold Coast",
     	"We would like to do <strong>your</strong> typing"
     ],
     typeSpeed: 0
