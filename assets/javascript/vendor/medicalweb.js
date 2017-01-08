3606ab1299ad4d7057fdadbca3380fbc5a6278a9
Jeremy Lynch <jeremylynch1994@gmail.com>
2017-01-08 19:41:02 +1000

    fixed viewport height stuff

--- assets/javascript/vendor/medicalweb.js
+++ assets/javascript/vendor/medicalweb.js
@@ -9,9 +9,4 @@ $(document).ready(function() {
     typeSpeed: 0
   });
 
-  viewport = $(window).height();
-  middle_cta_height = viewport/2
-
-  $('.middle-cta').css('height', middle_cta_height)
-
 });
