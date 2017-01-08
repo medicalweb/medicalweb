94406478eab74abcfed495463d77f1b51d0faa57
Jeremy Lynch <jeremylynch1994@gmail.com>
2017-01-08 19:40:23 +1000

    added contact details

--- assets/javascript/vendor/medicalweb.js
+++ assets/javascript/vendor/medicalweb.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 	$('.typed-content').typed({
-    strings: ["Hi.", "We are a group of Medical Students"],
+    strings: [
+    	"Hi.", 
+    	"We are a group of Medical Students", 
+    	"At Griffith University, Gold Coast",
+    	"We would like to do <strong>your</strong> typing"
+    ],
     typeSpeed: 0
   });
 
