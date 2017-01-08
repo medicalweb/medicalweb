a7169e5fc141836a7dd299375eff4ce466f78334
Jeremy Lynch <jeremylynch1994@gmail.com>
2017-01-08 19:11:58 +1000

    fixed up busy doctors section

--- assets/javascript/vendor/medicalweb.js
+++ assets/javascript/vendor/medicalweb.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
     strings: ["Hi.", "We are a group of Medical Students"],
     typeSpeed: 0
   });
+
+  viewport = $(window).height();
+  middle_cta_height = viewport/2
+
+  $('.middle-cta').css('height', middle_cta_height)
+
 });
