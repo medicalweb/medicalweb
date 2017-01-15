42610faae50668ec5b561fe3be799d463332b0e3
Jeremy Lynch <jeremylynch1994@gmail.com>
2017-01-15 10:24:11 +1000

    fix the fullpage path

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,4 +3,4 @@
 
 {% include_relative vendor/typed.min.js %}
 {% include_relative vendor/medicalweb.js %}
-{% include_relative vendor/jquery.fullpage.min.js %}
+{% include_relative vendor/jquery.fullPage.min.js %}
